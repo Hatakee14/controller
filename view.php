3aa0277209acb99246d67369fcc07d6d895f2fdc
Namanya hatake tapi aslinya nafi <118970290+Hatakee14@users.noreply.github.com>
2024-05-14 19:58:05 +0700

    Create view.php

---
+++ view.php
@@ -0,0 +1,29 @@
+ <form action="simpan_produksi" method="POST" enctype="multipart/form-data" >
+          
+                          <label for="name" class="form-label">Nama Barang</label>
+                          <input name="nama_barang" type="text" class="form-control" placeholder="Nama barang">
+                      
+
+       
+                          <label for="name" class="form-label">Tanggal Mulai</label>
+                          <input name="tanggal_mulai" type="date" class="form-control" placeholder="Tanggal Mulai">
+                    
+
+
+                          <label for="name" class="form-label">Tanggal Selesai</label>
+                          <input name="tanggal_selesai" type="date" class="form-control" placeholder="Tanggal Selesai">
+          
+
+
+                          <label for="name" class="form-label">Jumlah Produksi</label>
+                          <input name="jumlah_produksi" type="number" class="form-control" placeholder="Jumlah Produksi">
+
+    <label for="name" class="form-label">Bahan Baku</label>
+    <input name="bahan_baku" type="text" class="form-control" placeholder="Bahan Baku">
+
+    <label for="file" class="form-label">Input File</label>
+    <input name="file" type="file" class="form-control" placeholder="pilih file">
+
+                  
+                        <button>simpan</button>
+                      </form>
